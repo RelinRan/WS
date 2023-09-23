b6ca2688192dbac503f7a3a9241e3282f7434304
冉瑞林 <1101005304@qq.com>
2023-09-23 16:18:51 +0800

    init

--- app/src/androidTest/java/androidx/ws/core/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/androidx/ws/core/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("androidx.ws.core", appContext.getPackageName());
+        assertEquals("androidx.ws", appContext.getPackageName());
     }
 }
